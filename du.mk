311ea8b6887cdc5dfe0a27d1bffbdc3501e8083a
althafvly <althafvly@gmail.com>
2018-05-28 15:18:29 +0530

    DU-ify

--- du.mk
+++ du.mk
@@ -22,8 +22,8 @@ TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := osprey
-PRODUCT_NAME := lineage_osprey
 PRODUCT_MODEL := MotoG3
+PRODUCT_NAME := du_osprey
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := osprey
