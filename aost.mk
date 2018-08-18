94543c45e383393b71ef475d273b5f9a2916c5e6
Ahmed Hady <ahmedhady6@gmail.com>
2018-08-18 03:10:36 +0200

    UltraAOST

--- aost.mk
+++ aost.mk
@@ -14,15 +14,14 @@
 # limitations under the License.
 
 $(call inherit-product, device/motorola/osprey/full_osprey.mk)
+$(call inherit-product, vendor/aost/config/common.mk)
 
 # Boot animation
-TARGET_BOOTANIMATION_HALF_RES := true
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
+TARGET_BOOT_ANIMATION_RES := 720
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := osprey
-PRODUCT_NAME := lineage_osprey
+PRODUCT_NAME := aost_osprey
 PRODUCT_MODEL := MotoG3
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
